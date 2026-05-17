cfcc8469c97b1877fe77dbebac5a7799a3f107c7
Joseph <sectoidman@a7m2.me>
2026-05-17 15:39:38 -0700

    Squashed commit of the following:
    commit b53b779bdc69d4e0831b180c803b1d72b01a0b2d
    Author: Joseph <sectoidman@a7m2.me>
    Date:   Sun May 17 15:35:20 2026 -0700
    
        oops
    
    commit f0beba4932fb1f959422ee18293f49b22cdabbc1
    Author: Joseph <sectoidman@a7m2.me>
    Date:   Sun May 17 15:32:57 2026 -0700
    
        make health check an optional feature

--- components/sen0322/sen0322.h
+++ components/sen0322/sen0322.h
@@ -13,11 +13,13 @@ class SEN0322Sensor : public PollingComponent, public i2c::I2CDevice, public sen
   void dump_config() override;
   void update() override;
   float get_setup_priority() const override;
+  void enable_health_check(bool enable) { enable_health_check_ = enable; }
 
  protected:
   float calibrated_value_;
   uint8_t version_ {0x0};
   uint8_t probeLife_ {0xFF};
+  bool enable_health_check_{false};
 
   void readFlash();
   void getVersion();
